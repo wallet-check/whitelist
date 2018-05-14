09494c1e84960f22f411d319cacf67c93a855bae
BeardPower <26968654+BeardPower@users.noreply.github.com>
2018-05-14 16:56:10 +0200

    Update index.js
    Added hash for testing for case-senitivity.

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ const org = {
 
 const whitelist = {
 	'6a193697dbf60ff09da428a8fc219cc7939173f256bbbdd4afebb2547062e1f8': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' },
-	'5E8DF4010ED719880037C5E9160EA147352713F0C25CD7C8622D949C0BF299C0': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' }
+	'5E8DF4010ED719880037C5E9160EA147352713F0C25CD7C8622D949C0BF299C0': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' },
+	'5e8df4010ed719880037c5e9160ea147352713f0c25cd7c8622d949c0bf299c0': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' },
 }
