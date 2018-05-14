75517a2bd7dca0f2aa7868f235fc94578e7955b6
BeardPower <26968654+BeardPower@users.noreply.github.com>
2018-05-14 16:51:57 +0200

    Update index.js
    Added the hash of the current wallet.

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ const org = {
 }
 
 const whitelist = {
-	'6a193697dbf60ff09da428a8fc219cc7939173f256bbbdd4afebb2547062e1f8': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' }
+	'6a193697dbf60ff09da428a8fc219cc7939173f256bbbdd4afebb2547062e1f8': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' },
+	'5E8DF4010ED719880037C5E9160EA147352713F0C25CD7C8622D949C0BF299C0': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' }
 }
