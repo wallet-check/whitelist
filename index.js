b6a726b2a691ad5da0d816ce3c692e5c1fad872d
FLuX LoOP <x8x@users.noreply.github.com>
2018-05-14 19:33:10 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const org = {
+		rl: {
+			name: 'Red Language',
+			url: 'https://www.red-lang.org/',
+			app: {
+				wallet: {
+					name: 'RED Wallet',
+					url: 'https://github.com/red/wallet'
+				}
+			}
+		}
+	}
+
+	const whitelist = {
+		'6a193697dbf60ff09da428a8fc219cc7939173f256bbbdd4afebb2547062e1f8': { o: org.rl, a: org.rl.app.wallet, v: '1.0.0' }
+	}
